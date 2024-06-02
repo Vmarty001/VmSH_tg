c473f5d88c5819dbf34bc243289e5fa595490be4
Vmarty001 <98187754+Vmarty001@users.noreply.github.com>
2024-06-02 22:21:45 +0300

    Update index.js
    nes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const token = '5826846570:AAFuYkjJ-2dEpvFRGwHCLatFxsrYl7r6Oig';
-const webAppUrl = 'https://neon-marzipan-000e89.netlify.app/';
+const webAppUrl = 'https://vmayshop.netlify.app/';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
