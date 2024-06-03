a9c9cfae5ac47f0a1220459b8619166fea8415b4
Vmarty001 <98187754+Vmarty001@users.noreply.github.com>
2024-06-03 03:11:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,18 +17,10 @@ bot.on('message', async (msg) => {
     const text = msg.text;
 
     if (text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        await bot.sendMessage(chatId, 'Переходи в наш магазин по кнопке ниже!', {
             reply_markup: {
                 keyboard: [
-                    [{ text: 'Заполнить форму', web_app: { url: webAppUrl } }]
-                ]
-            }
-        });
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
+                    [{ text: 'Открыть магазин', web_app: { url: webAppUrl } }]
                 ]
             }
         });
